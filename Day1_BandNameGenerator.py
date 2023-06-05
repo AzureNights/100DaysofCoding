2db70e04d6b4e505ce1c48ba8df14bc6e93ad747
AzureNights <106224470+AzureNights@users.noreply.github.com>
2023-06-05 10:28:22 +0800

    Trying out Git CodeSpaces

--- Day1_BandNameGenerator.py
+++ Day1_BandNameGenerator.py
@@ -4,3 +4,4 @@ fav_activity = input("What's your favourite activity?\n")
 fav_animal = input("What's your favourite animal?\n")
 
 print("Your band name is " + fav_activity + " " + fav_animal + "!")
+
